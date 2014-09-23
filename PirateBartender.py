c961b5bc6587a35616712ecca9bd6cc65aa5c3e7
mrbenji <benji.tittle@gmail.com>
2014-09-23 13:43:07 -0700

    Added multiple customers, limited ingredients.

--- PirateBartender.py
+++ PirateBartender.py
@@ -16,9 +16,16 @@ ingredients = {
     "fruity": ["durian puree", "slice of orange", "dash of cassis", "cherry on top"]
 }
 
+stockUsed = {}
+
+for style in ingredients:
+    for ingredient in ingredients[style]:
+        stockUsed[ingredient] = random.randint(1,6)
+
 adjectives = ["Furry","Balding","Slutty","Shiny","Uncultured"]
 nouns = ["Dingo","Jackhammer","Navel", "Superhero","Doughnut"]
 
+customers = {}
 
 def askStyle(styleDict):
     
@@ -36,32 +43,35 @@ def constructDrink(styleDict):
     drink = []
     
     for style in styleDict.keys():
+
         if styleDict[style] == True:
-            drink.append( random.choice(ingredients[style]) )
+
+            randomChoice = random.choice(ingredients[style])
+
+            if stockUsed[randomChoice] == 0:
+                print "\n Arrr... be right back, need to refill my", randomChoice, "supply."
+                stockUsed[randomChoice] == random.randint(1,5)
+            else:
+                stockUsed[randomChoice] -= 1
+
+            drink.append(randomChoice)
+            if randomChoice in stockUsed:
+                stockUsed 
 
     return drink
 
-
-if __name__ == '__main__':
-    
-    styleDict = {}
-    numDrunk = 0
-
-    print "\nAhoy thar... let's find a new drink for ye!"
+def serveCustomer(customer_name):
 
     while True:
 
-        if numDrunk > 4:
-            print "\nYer too drunk to even walk the plank... yer cut off!"
+        if customers[customer_name]["numDrunk"] > 4:
+            print "\nYar too drunk to even walk the plank... yar cut off!"
             break
 
-        drink = constructDrink(askStyle(styleDict))
-
-        if len(drink) > 1:
-            numDrunk += 1
-            print '\nHere be yer',
-            print random.choice(adjectives), random.choice(nouns) + ":"
-            for i in drink:
+        if len( customers[customer_name]["regDrinkIngredients"] ) > 1:
+            customers[customer_name]["numDrunk"] += 1
+            print '\nHere be yer', customers[customer_name]["regDrinkName"]
+            for i in customers[customer_name]["regDrinkIngredients"]:
                 print " -", i
         else: 
             print "\nYar a picky one!  No drink for ye, then."
@@ -73,10 +83,37 @@ if __name__ == '__main__':
             if another.lower() in ("y","yes","n","no"):
                 break
 
-        if another.lower() in ("y","yes"):
-            print "\nLet's see what grog suits yer fancy this time."
-
         if another.lower() in ("n","no"):
             print ("\nOff with ye then, ye scallywag!")
             break
+
+    return
+
+
+if __name__ == '__main__':
+    
+    styleDict = {}
+
+    while True:
+        customer_name = raw_input("\nAhoy thar... what be yar name? ")
+        if customer_name not in customers:
+            print "\nWell, " + customer_name + "... let's find a new drink for ye!"
+            customers[customer_name]={}
+            customers[customer_name]["numDrunk"]=0
+            customers[customer_name]["regDrinkName"]=random.choice(adjectives) + " " + random.choice(nouns)
+            customers[customer_name]["regDrinkIngredients"]=constructDrink(askStyle(styleDict))
+
+        serveCustomer(customer_name) 
+
+        while True: 
+            print
+            another = raw_input('Is thar another customer to serve? ')
+            if another.lower() in ("y","yes","n","no"):
+                break
+
+        if another.lower() in ("n","no"):
+            print ("\nI'm closing up, then... be gone with ye all!")
+            break
+
+
 
