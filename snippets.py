9212a212f10acf0776da3c96ceedac7bb83bc09e
mrbenji <benji.tittle@gmail.com>
2014-09-30 13:46:44 -0700

    3rd commit of provided code.

--- snippets.py
+++ snippets.py
@@ -20,6 +20,16 @@ def make_parser():
     description = "Store and retrieve snippets of text"
     parser = argparse.ArgumentParser(description=description)
 
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    put_parser.add_argument("filename", default="snippets.csv", nargs="?",
+        help="The snippet filename")
+
     return parser
 
 def main():
@@ -27,6 +37,11 @@ def main():
     logging.info("Starting snippets")
     parser = make_parser()
     arguments = parser.parse_args(sys.argv[1:])
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
 
 if __name__ == "__main__":
     main()
