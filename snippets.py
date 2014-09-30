9f2c059b9d3f8b15c613753e9fc92fbd3639b0be
mrbenji <benji.tittle@gmail.com>
2014-09-30 16:04:15 -0700

    version given as starting point for project

--- snippets.py
+++ snippets.py
@@ -1,15 +1,18 @@
-import argparse, logging, csv, sys
+import sys
+import argparse
+import logging
+import csv
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
 def put(name, snippet, filename):
     """ Store a snippet with an associated name in the CSV file """
-    logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
+    logging.info("Writing {}:{} to {}".format(name, snippet, filename))
     logging.debug("Opening file")
     with open(filename, "a") as f:
         writer = csv.writer(f)
-        logging.debug("Writing snippet to file")
+        logging.debug("Writing snippet to file".format(name, snippet))
         writer.writerow([name, snippet])
     logging.debug("Write successful")
     return name, snippet
@@ -37,11 +40,13 @@ def main():
     logging.info("Starting snippets")
     parser = make_parser()
     arguments = parser.parse_args(sys.argv[1:])
+    # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
     command = arguments.pop("command")
 
     if command == "put":
         name, snippet = put(**arguments)
+        print "Stored '{}' as '{}'".format(snippet, name)
 
 if __name__ == "__main__":
     main()
