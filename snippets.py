23f278e9bea6426ea511e15d689fff9ff10ece8b
mrbenji <benji.tittle@gmail.com>
2014-09-30 17:40:54 -0700

    get() is functioning, currently returns None, None if no snippet by that name.

--- snippets.py
+++ snippets.py
@@ -6,13 +6,28 @@ import csv
 # Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
 
+
+def get(name, filename):
+    """ Print a snippet from a CSV file, based on an associated name. """
+    logging.info("Reading {} from {}".format(name, filename))
+    logging.debug("Opening a file")
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        logging.debug("Reading snippet from file")
+        for line in reader:
+            for key in line:
+                if key == name:
+                    logging.debug("Read successful")
+                    return name, line[key]
+    return None, None
+
 def put(name, snippet, filename):
     """ Store a snippet with an associated name in the CSV file """
     logging.info("Writing {}:{} to {}".format(name, snippet, filename))
     logging.debug("Opening file")
     with open(filename, "a") as f:
         writer = csv.writer(f)
-        logging.debug("Writing snippet to file".format(name, snippet))
+        logging.debug("Writing snippet to file")
         writer.writerow([name, snippet])
     logging.debug("Write successful")
     return name, snippet
@@ -24,6 +39,13 @@ def make_parser():
     parser = argparse.ArgumentParser(description=description)
 
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Print a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+    get_parser.add_argument("filename", default="snippets.csv", nargs="?",
+        help="The snippet filename")
 
     # Subparser for the put command
     logging.debug("Constructing put subparser")
@@ -43,6 +65,10 @@ def main():
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
     command = arguments.pop("command")
+
+    if command == "get":
+        name, snippet = get(**arguments)
+        print "Retrieved '{}', keyed to '{}'".format(snippet, name)
 
     if command == "put":
         name, snippet = put(**arguments)
