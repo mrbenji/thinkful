7db6f59de5e3e06f4d206a007868ad69b251e167
mrbenji <benji.tittle@gmail.com>
2014-10-07 10:06:14 -0700

    tiny reformat

--- tweetful/authorization.py
+++ tweetful/authorization.py
@@ -6,6 +6,7 @@ from requests_oauthlib import OAuth1
 from secret import CLIENT_KEY, CLIENT_SECRET
 from urls import *
 
+
 def get_request_token():
     """ Get a token allowing us to request user authorization """
     oauth = OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET)
@@ -16,6 +17,7 @@ def get_request_token():
     request_token = credentials.get("oauth_token")[0]
     request_secret = credentials.get("oauth_token_secret")[0]
     return request_token, request_secret
+
 
 def authorize():
     """ A complete OAuth authentication flow """
