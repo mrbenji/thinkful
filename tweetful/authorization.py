c05af00f0dc716e014dd57fafc7c450cadfd487d
mrbenji <benji.tittle@gmail.com>
2014-10-07 09:06:21 -0700

    first round of spoon-fed staging

---
+++ tweetful/authorization.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import urlparse
+
+import requests
+from requests_oauthlib import OAuth1
+
+from secret import CLIENT_KEY, CLIENT_SECRET
+from urls import *
+
+def get_request_token():
+    """ Get a token allowing us to request user authorization """
+    oauth = OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET)
+    response = requests.post(REQUEST_TOKEN_URL,
+                             auth=oauth)
+    credentials = urlparse.parse_qs(response.content)
+
+    request_token = credentials.get("oauth_token")[0]
+    request_secret = credentials.get("oauth_token_secret")[0]
+    return request_token, request_secret
+
+def authorize():
+    """ A complete OAuth authentication flow """
+    request_token, request_secret = get_request_token()
+    print request_token, request_secret
+
+
+def get_user_authorization(request_token):
+    """
+    Redirect the user to authorize the client, and get them to give us the
+    verification code.
+    """
+    authorize_url = AUTHORIZE_URL
+    authorize_url = authorize_url.format(request_token=request_token)
+    print 'Please go here and authorize: ' + authorize_url
+    return raw_input('Please input the verifier: ')
---
+++ tweetful/tweetful.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import authorization
+
+
+def main():
+    """ Main function """
+    authorization.authorize()
+
+if __name__ == "__main__":
+    main()
