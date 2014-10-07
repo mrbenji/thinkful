ceab55e762de061ef8f66d8c76750ca1568c932d
mrbenji <benji.tittle@gmail.com>
2014-10-07 13:06:28 -0700

    adding get_access_token()

--- tweetful/authorization.py
+++ tweetful/authorization.py
@@ -32,3 +32,20 @@ def get_user_authorization(request_token):
     authorize_url = authorize_url.format(request_token=request_token)
     print 'Please go here and authorize: ' + authorize_url
     return raw_input('Please input the verifier: ')
+
+
+def get_access_token(request_token, request_secret, verifier):
+    """"
+    Get a token which will allow us to make requests to the API
+    """
+    oauth = OAuth1(CLIENT_KEY,
+                   client_secret=CLIENT_SECRET,
+                   resource_owner_key=request_token,
+                   resource_owner_secret=request_secret,
+                   verifier=verifier)
+
+    response = requests.post(ACCESS_TOKEN_URL, auth=oauth)
+    credentials = urlparse.parse_qs(response.content)
+    access_token = credentials.get('oauth_token')[0]
+    access_secret = credentials.get('oauth_token_secret')[0]
+    return access_token, access_secret
