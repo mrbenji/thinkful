df6d2b08b99e855a1ccfecbb984154a9ac86a772
mrbenji <benji.tittle@gmail.com>
2014-10-23 13:42:10 -0700

    added some mixed tests

--- discount_calculator/test_discount_calculator.py
+++ discount_calculator/test_discount_calculator.py
@@ -12,5 +12,9 @@ class DiscountCalculatorTests(unittest.TestCase):
     def test_zero_absolute_discount(self):
         self.assertTrue(100,calculate_discount(200, 50, 0))
 
+    def test_mixed_discounts(self):
+        self.assertTrue(140, calculate_discount(200, 25, 10))
+        self.assertTrue(119.75, calculate_discount(195.44, 33.33, 10.55))
+
 if __name__ == "__main__":
     unittest.main()
